42349146e1d030b9f59d5c276bdd133a12a51ce5
LGZeldner <lgzeldner@gmail.com>
2020-07-10 01:17:46 +0300

    Admin Config

--- src/app/admin-config/admin-config.component.ts
+++ src/app/admin-config/admin-config.component.ts
@@ -11,6 +11,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AdminConfigComponent implements OnInit {
   formAdd: FormGroup;
   films;
+  id:number;
   disabled = false;
   constructor(private activeRoute: ActivatedRoute,
               private router: Router,
@@ -19,9 +20,9 @@ export class AdminConfigComponent implements OnInit {
   }
   ngOnInit() {
     this.updateData();
-    this.formAdd = new FormGroup({                              /**создание новой формы для заказа фильм или сериалов из списка готовых */
+    this.formAdd = new FormGroup({                              /**создание новой формы для заказа фильм или сериалов из списка*/
       name: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
-      picture: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
+      picture: new FormControl( { value: '', disabled: this.disabled }),
       info: new FormControl( { value: '', disabled: this.disabled } , [Validators.required]),
       price: new FormControl({ value: '', disabled: this.disabled }, [Validators.required]),
     });
@@ -41,8 +42,37 @@ export class AdminConfigComponent implements OnInit {
   }
   onSubmit(){
     console.log(this.formAdd.value);
-    this.FilmsService.postFilms(this.formAdd.value);
-    this.router.navigate(['/admin-change']);
+    if (this.id== null){
+      this.FilmsService.postFilms(this.formAdd.value);
+      this.router.navigate(['/admin-config']);
+    }
+    else {
+      this.FilmsService.putById(this.id, this.formAdd.value);
+    }
+    this.updateData();
+    this.formAdd = new FormGroup({                              /**создание новой формы для заказа фильм или сериалов из списка*/
+      name: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
+      picture: new FormControl( { value: '', disabled: this.disabled }),
+      info: new FormControl( { value: '', disabled: this.disabled } , [Validators.required]),
+      price: new FormControl({ value: '', disabled: this.disabled }, [Validators.required]),
+    });
 
+  }
+
+  delete(id:number) {
+    console.log(id);
+    this.FilmsService.deleteById(id);
+    this.updateData();
+  }
+
+  edit(film){
+    console.log(film);
+    this.formAdd = new FormGroup({                              /**создание новой формы для заказа фильм или сериалов из списка*/
+      name: new FormControl( { value: film.name, disabled: this.disabled }, [Validators.required]),
+      picture: new FormControl( { value: film.picture, disabled: this.disabled }),
+      info: new FormControl( { value: film.info, disabled: this.disabled } , [Validators.required]),
+      price: new FormControl({ value: film.price, disabled: this.disabled }, [Validators.required]),
+    });
+    this.id = film.id;
   }
 }
